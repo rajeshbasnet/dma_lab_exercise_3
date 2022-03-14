7cb44f74ba961e2de4463372e4836b1ce6f30848
rajeshbasnet01 <brajesh18@tbc.edu.np>
2022-03-14 13:06:57 +0545

    Completed all tasks, coding challenges, homework from lab ex 3

---
+++ ScrollingText/app/src/main/java/com/example/scrollingtext/Task1.java
@@ -0,0 +1,14 @@
+package com.example.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task1);
+    }
+}
